d1b2ccba12d674ec6ca436c07b6ca139d4043350
fine <dlgmltjr0925@gmail.com>
2022-01-06 21:19:04 +0900

    feature: Swagger library 추가 및 document 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
-import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const globalPrefix = process.env.GLOBAL_PREFIX;
+  app.setGlobalPrefix(globalPrefix);
+
+  if (process.env.NODE_ENV !== 'production') {
+    const documentBuilder = new DocumentBuilder()
+      .setTitle('Recipe4m Auth API')
+      .setDescription(`Recipe4m Auth API for ${process.env.NODE_ENV}`)
+      .setVersion('1.0')
+      .setExternalDoc('JSON Specification', '/swagger-ui-json')
+      .addBearerAuth();
+
+    const config = documentBuilder.build();
+
+    const document = SwaggerModule.createDocument(app, config, {
+      extraModels: [],
+    });
+
+    const swaggerPath = `${globalPrefix}/swagger-ui`;
+    SwaggerModule.setup(swaggerPath, app, document);
+  }
+
   await app.listen(3000);
 }
 bootstrap();
