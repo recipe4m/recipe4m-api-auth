abbf77427d3301e0875d8c808ba32dfba2161189
fine <dlgmltjr0925@gmail.com>
2022-01-06 21:05:22 +0900

    feature: response time middle ware 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { ResponseTimeLoggerMiddleware } from './middlewares/reponse-time-logger.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ResponseTimeLoggerMiddleware).forRoutes('*');
+  }
+}
