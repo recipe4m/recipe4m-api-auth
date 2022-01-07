7e61a6ebacf9b437021e9ced495a5a9b737063ec
fine <dlgmltjr0925@gmail.com>
2022-01-07 22:09:21 +0900

    feature: Auth controller 응답 스키마 정의

---
+++ src/auth/schemas/api-post-auth-refresh-res-data.schema.ts
@@ -0,0 +1,3 @@
+import { TokensDto } from '@auth/dto/tokens.dto';
+
+export class ApiPostAuthRefreshResData extends TokensDto {}
---
+++ src/auth/schemas/api-post-auth-sign-in-res-data.schema.ts
@@ -0,0 +1,3 @@
+import { TokensDto } from '@auth/dto/tokens.dto';
+
+export class ApiPostAuthSignInResData extends TokensDto {}
