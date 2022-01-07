5672ecfe364db6ae81d025a55ead54c63400afac
fine <dlgmltjr0925@gmail.com>
2022-01-07 15:45:36 +0900

    feature: sign up 기능 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,16 @@
 import {
   BadRequestException,
+  ConflictException,
   ForbiddenException,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
 
+import { ApiAlreadyRegisteredError } from '@http-exceptions/api-already-registered-error';
 import { ApiNotFoundError } from '@src/libs/http-exceptions/api-not-found-error';
 import { HttpExceptionFilter } from '@filters/http-exception.filter';
 import { OAuthService } from './o-auth.service';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from '@src/prisma/prisma.service';
 import { RefreshDto } from './dto/refresh.dto';
 import { SignInDto } from './dto/sign-in.dto';
@@ -18,6 +21,9 @@ import { TokenService } from './token.service';
 @Injectable()
 export class AuthService {
   static ErrorNotFoundUser = new ApiNotFoundError('사용자를 찾을 수 없습니다.');
+  static ErrorAlreadyRegistered = new ApiAlreadyRegisteredError(
+    '이미 등록된 회원입니다.',
+  );
 
   constructor(
     private readonly prismaService: PrismaService,
@@ -32,9 +38,12 @@ export class AuthService {
       if (!payload)
         throw new BadRequestException(TokenService.ErrorInvalidToken);
 
+      const { provider } = signInDto;
+      const oAuthId = payload.sub;
+
       const { user } = await this.prismaService.authInfo.findFirst({
         select: { user: true },
-        where: { ...signInDto },
+        where: { provider, oAuthId },
         orderBy: { id: 'desc' },
       });
 
@@ -53,7 +62,42 @@ export class AuthService {
   }
 
   async signUp(signUpDto: SignUpDto) {
-    return null;
+    try {
+      const payload = await this.oAuthService.verifyToken(signUpDto);
+
+      if (!payload)
+        throw new BadRequestException(TokenService.ErrorInvalidToken);
+
+      const { provider } = signUpDto;
+      const oAuthId = payload.sub;
+      const name = payload.name || '';
+      const profileImageUrl = payload.picture || '';
+
+      const user = await this.prismaService.user.create({
+        select: { id: true },
+        data: {
+          name,
+          profileImageUrl,
+          authInfo: {
+            create: [
+              {
+                provider,
+                oAuthId,
+              },
+            ],
+          },
+        },
+      });
+
+      return await this.tokenService.issueTokens({ userId: user.id });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ConflictException(AuthService.ErrorAlreadyRegistered);
+        }
+      }
+      throw error;
+    }
   }
 
   async signOut({ refreshToken }: SignOutDto) {
