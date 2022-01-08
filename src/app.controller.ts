32d1362323495dbee3bef000ebc8f31314c3dd73
fine <dlgmltjr0925@gmail.com>
2022-01-08 10:55:38 +0900

    feature: healthy 경로 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('healthy')
+  @Get('auth/healthy')
   @ApiOkResponse({ type: String })
   healthy(): string {
     return this.appService.healthy();
